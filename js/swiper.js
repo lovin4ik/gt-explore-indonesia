d6ba34433b43afebb20c30ecd35145b6055bbe50
lovin4ik <rasteniaros@gmail.com>
2024-09-18 18:46:04 +0300

    fix

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,18 @@
 const swiper = new Swiper('.swiper', {
 	slidesPerView: 1,
 	speed: 500,
+	spaceBetween: 31,
 	autoplay: {
 		delay: 5000
 	},
 
 	breakpoints: {
 		1400: {
-			slidesPerView: 3
+			slidesPerView: 3,
+			spaceBetween: 0
 		},
-		1320: {
+		1200: {
+			spaceBetween: 0,
 			slidesPerView: 2
 		}
 	},
